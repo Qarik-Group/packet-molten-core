156ee03494e9db9c6aefee09709485bd28ff2c32
ramonskie <ramonmakkelie@gmail.com>
2019-09-30 11:49:58 +0200

    refactor to use moltencore

--- cluster.tf
+++ cluster.tf
@@ -2,10 +2,6 @@ provider "packet" {
   auth_token = "${var.packet_api_key}"
 }
 
-provider "ct" {
-  version = "0.4.0"
-}
-
 resource "tls_private_key" "ssh_key" {
   algorithm   = "RSA"
 }
@@ -16,72 +12,14 @@ resource "packet_project_ssh_key" "ssh_key" {
   project_id = "${var.packet_project_id}"
 }
 
-resource "tls_private_key" "docker_key" {
-  algorithm   = "RSA"
-}
-
-resource "tls_self_signed_cert" "docker_ca" {
-  key_algorithm   = "RSA"
-  private_key_pem = "${tls_private_key.docker_key.private_key_pem}"
-
-  subject {
-    common_name = "docker"
-  }
-
-  validity_period_hours = 43800
-  is_ca_certificate = true
-
-  allowed_uses = [
-    "cert_signing",
-  ]
-}
-
-resource "tls_cert_request" "docker_client_csr" {
-  key_algorithm   = "RSA"
-  private_key_pem = "${tls_private_key.docker_key.private_key_pem}"
-
-  subject {
-    common_name  = "client"
-  }
-}
-
-resource "tls_locally_signed_cert" "docker_client_cert" {
-  cert_request_pem   = "${tls_cert_request.docker_client_csr.cert_request_pem}"
-  ca_key_algorithm   = "RSA"
-  ca_private_key_pem = "${tls_private_key.docker_key.private_key_pem}"
-  ca_cert_pem        = "${tls_self_signed_cert.docker_ca.cert_pem}"
-
-  validity_period_hours = 43800
-
-  allowed_uses = [
-    "client_auth",
-  ]
-}
-
-data "template_file" "container-linux-config" {
-  template = file("${path.module}/templates/clc.yaml")
-  count = "${var.node_count}"
+data "template_file" "ignition-config" {
+  template = file("${path.module}/templates/config.ign")
 
   vars = {
-    index = count.index
-    flannel_cidr = cidrsubnet("10.1.0.0/16", 8, count.index + 1 )
-    flannel_cidr_dashed = replace(cidrsubnet("10.1.0.0/16", 8, count.index + 1 ), "/", "-")
     discovery_url = "${file(var.discovery_url_file)}"
-    docker_ca = "${tls_self_signed_cert.docker_ca.cert_pem}"
-    docker_key = "${tls_private_key.docker_key.private_key_pem}"
-    docker_client_cert = "${tls_locally_signed_cert.docker_client_cert.cert_pem}"
   }
 
   depends_on = [template_file.etcd_discovery_url]
-}
-
-data "ct_config" "container-linux-config" {
-  count = "${var.node_count}"
-  content      = "${element(data.template_file.container-linux-config.*.rendered, count.index)}"
-  platform     = "packet"
-  pretty_print = false
-
-  depends_on = [data.template_file.container-linux-config]
 }
 
 resource "template_file" "etcd_discovery_url" {
@@ -93,12 +31,12 @@ resource "template_file" "etcd_discovery_url" {
 }
 
 resource "packet_device" "node" {
-  hostname         = "${format("node-z%01d.bare-metal.cf", count.index)}"
+  hostname         = "${format("node-z%01d.molten-core", count.index)}"
   operating_system = "coreos_stable"
   plan             = "${var.node_type}"
 
   count            = "${var.node_count}"
-  user_data        = "${element(data.ct_config.container-linux-config.*.rendered, count.index)}"
+  user_data        = "${data.template_file.ignition-config.rendered}"
   facilities       = ["${var.packet_facility}"]
   project_id       = "${var.packet_project_id}"
   billing_cycle    = "hourly"
@@ -108,23 +46,6 @@ resource "packet_device" "node" {
 output "nodes" {
   value = ["${packet_device.node.*.access_public_ipv4}"]
 }
-
-output "flannel_cidrs" {
-  value = ["${data.template_file.container-linux-config.*.vars.flannel_cidr}"]
-}
-
-output "docker_client_cert" {
-  value = "${tls_locally_signed_cert.docker_client_cert.cert_pem}"
-}
-
-output "docker_ca" {
-  value = "${tls_self_signed_cert.docker_ca.cert_pem}"
-}
-
-output "docker_key" {
-  value = "${tls_private_key.docker_key.private_key_pem}"
-}
-
 
 output "ssh_key" {
   value = "${tls_private_key.ssh_key.private_key_pem}"
